3e2f7958c27f8ad0312efdb36e382cd78b7a6a7b
nuntanunta5153 <nuntanunta5153@gmail.com>
2021-05-27 01:23:49 +0900

    編集機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,10 +17,14 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
 
   def update
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to post
   end
 
   def destroy
